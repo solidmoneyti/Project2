af36a5be631a463a2dd4ec650bf742d818c8cd78
Thinh <thinh1198@hotmail.com>
2024-03-14 18:59:17 +0000

    chart file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <TotalChart />
     </>
   )
 }
---
+++ src/components/chart/chart.jsx
@@ -0,0 +1,11 @@
+
+function TotalChart() {
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default TotalChart;
