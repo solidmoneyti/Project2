e47ac56c2532abbea814d7e9dd5194b8bdfc6597
jermaine <jblearn2023@gmail.com>
2024-03-17 12:14:52 +0000

    append table

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import Navbar from './component/Navbar/index';
 import ExpenseModal from './component/table/ExpenseModal';
 import SecondRow from './component/rows/SecondRow';
 import ThirdRow from './component/rows/ThirdRow';
+import DataRow from './component/rows/dataRow';
 
 const App = () => {
-  //FOR EXPENSES
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [expenseName, setExpenseName] = useState('');
   const [expenseAmount, setExpenseAmount] = useState('');
   const [expenseDate, setExpenseDate] = useState('');
+  const [tableData, setTableData] = useState([]); // State to manage table data
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -21,14 +22,25 @@ const App = () => {
   };
 
   const handleAddExpense = () => {
-    // expense output
-    console.log("Expense Name:", expenseName);
-    console.log("Expense Amount:", expenseAmount);
-    console.log("Expense Date:", expenseDate);
-
-    // Close the modal after adding expense
+    console.log("Adding expense:", expenseName, expenseAmount, expenseDate);
+    // Add the new expense to table data
+    const newExpense = {
+      name: expenseName,
+      amount: expenseAmount,
+      date: expenseDate
+    };
+    console.log("New expense:", newExpense);
+    setTableData([...tableData, newExpense]);
+  
+    // Clear input fields
+    setExpenseName('');
+    setExpenseAmount('');
+    setExpenseDate('');
+    console.log("Table data after adding:", tableData);
+    // Close the modal
     handleCloseModal();
   };
+  
 
   return (
     <div className="bg-gray-100 h-screen">
@@ -42,6 +54,10 @@ const App = () => {
   
         {/* Second Row */}
         <SecondRow handleOpenModal={handleOpenModal} />
+        
+        {/* DataRow component */}
+        <DataRow tableData={tableData} />
+
   
         {/* Third Row */}
         <ThirdRow />
