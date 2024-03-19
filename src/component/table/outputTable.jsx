feeed4f5af7e28f7a99bfb3750dfbd7f53838db1
Andsn1 <152194109+Andsn1@users.noreply.github.com>
2024-03-19 20:40:55 +0000

    updated the table for an improved table

--- src/component/table/outputTable.jsx
+++ src/component/table/outputTable.jsx
@@ -1,30 +1,27 @@
 
 const Table () => {
- <div> 
- <table
-  <caption class="caption-top">
-    Table 3.1: Professional wrestlers and their signature moves.
-  </caption>
+  <div>
+  <table class="border-separate border border-slate-500 ...">
   <thead>
     <tr>
-      <th>Wrestler</th>
-      <th>Signature Move(s)</th>
+      <th class="border border-slate-600 ...">State</th>
+      <th class="border border-slate-600 ...">City</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td>"Stone Cold" Steve Austin</td>
-      <td>Stone Cold Stunner, Lou Thesz Press</td>
+      <td class="border border-slate-700 ...">Indiana</td>
+      <td class="border border-slate-700 ...">Indianapolis</td>
     </tr>
     <tr>
-      <td>Bret "The Hitman" Hart</td>
-      <td >The Sharpshooter</td>
+      <td class="border border-slate-700 ...">Ohio</td>
+      <td class="border border-slate-700 ...">Columbus</td>
     </tr>
     <tr>
-      <td>Razor Ramon</td>
-      <td>Razor's Edge, Fallaway Slam</td>
+      <td class="border border-slate-700 ...">Michigan</td>
+      <td class="border border-slate-700 ...">Detroit</td>
     </tr>
   </tbody>
-</table> 
+</table>
 </div>
 }
