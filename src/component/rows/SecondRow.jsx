a4e9c109cafcb5a85fbda5fb3d8f23db4b956bd8
Andsn1 <152194109+Andsn1@users.noreply.github.com>
2024-03-22 19:38:28 +0000

    made changes to the second row

--- src/component/rows/SecondRow.jsx
+++ src/component/rows/SecondRow.jsx
@@ -5,11 +5,11 @@ const SecondRow = ({ handleOpenModal }) => {
     <div className="row2 flex justify-center">
         <div className="w-full bg-sky-950 p-4 rounded shadow-lg flex justify-evenly text-center">
           {/* Button in the center of Column 1 */}
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleOpenModal}>
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" onClick={handleOpenModal}>
             + Income/Expense
           </button>
           {/* Button in the center of Column 2 */}
-          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
             Cost of living search
           </button>
               </div>
